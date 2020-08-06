e92ce2b65bfada6d20c6909dd2618f9cae7264df
jflatorreg <jflatorreg@unal.edu.co>
2020-08-06 10:16:41 -0500

    Inicial commit

---
+++ funcs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 12:14:25 2020
+
+@author: juanfe
+"""
+
+def imprimir_mensaje(mensaje):
+    """
+    Imprime un mensaje
+    
+    :param mensaje: str Contiene el mensaje
+    :returns: None
+    """
+    print(mensaje)
+    return None
+
+def suma_nums(a,b):
+    """
+    Suma dos valores
+    
+    :param a: float Primer valor
+    :param b: float Segundo valor
+    :returns: float a+b        
+    """
+    return a+b
+
